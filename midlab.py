dbe95df4cccc67d1b66729953ebd905ae79e737b
BCS950 <mds392221@gmail.com>
2024-06-08 14:37:05 +0600

    Add files via upload

---
+++ midlab.py
@@ -0,0 +1,7 @@
+a = input("What is your name: ")
+w = input("What is your age: ")
+e = input("What is your favorite color: ")
+r = input("What is your favorite food: ")
+
+print(f"My name is {a}, Iam {w} years old, my favorite color is {e} and my favorite fruit is {r}")
+
