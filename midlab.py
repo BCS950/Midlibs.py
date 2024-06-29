0a79e09b5ce3282f7e4e1308b588d2b4e87e52ed
✓Easin Hossain <mds392221@gmail.com>
2024-06-29 21:04:54 +0600

    Update midlab.py

--- midlab.py
+++ midlab.py
@@ -1,7 +1,1 @@
-a = input("What is your name: ")
-w = input("What is your age: ")
-e = input("What is your favorite color: ")
-r = input("What is your favorite food: ")
-
-print(f"My name is {a}, Iam {w} years old, my favorite color is {e} and my favorite fruit is {r}")
 
